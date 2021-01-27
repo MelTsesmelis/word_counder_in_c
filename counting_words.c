7063cf4892622167d9b8c2d269d2f2dde2f49a7e
MelTsesmelis <75441621+MelTsesmelis@users.noreply.github.com>
2021-01-27 22:43:59 +0200

    Add files via upload

---
+++ counting_words.c
@@ -0,0 +1,156 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <sys/file.h>
+#include <unistd.h>
+#include <string.h>
+#include <error.h>
+
+#define NTHREADS 8
+
+//make a struct to take number of thread and the buffer(so what it has to read)
+typedef struct dataS data;
+struct dataS
+{
+    char *buffer;//we use the buffer to take the characters
+    int thread_counter;//to save the sum of threads
+};
+
+//initialize mutex 
+pthread_mutex_t mutex=PTHREAD_MUTEX_INITIALIZER;
+
+//initialize the total sum for the threads of one file
+int total_number=0;
+
+
+
+// DECLARE FUNCTION - IMPLEMENTATION IS ON END
+
+
+    //check if the given character is one from the separators of a word
+int word_seperator(char);
+    //counts the words of a file with the help of the threads
+void *counter_words_of_file(void *);
+
+
+
+//MAIN FUNCTION
+int main(int argc, char *argv[])
+{
+    //initialize the array of the threads
+    pthread_t threads[NTHREADS];
+    //initialize the struct of the arguments that every thread must has
+    data count[NTHREADS];
+   
+    
+    //create threads
+    for (int i = 0; i < NTHREADS; i++)
+    {
+        //first data   is the characters from the file
+        count[i].buffer=argv[2];
+        //second data is the number of thread
+        count[i].thread_counter=i;
+        pthread_create(&threads[i], NULL, &counter_words_of_file, (void *)&(count[i]));
+            
+    }
+    
+    //wait the threads to complete
+    for (int i = 0; i < NTHREADS; i++)
+    {
+        pthread_join(threads[i],NULL);
+    }
+
+    //calculate the size of output file 
+    int size_of_output_file=(sizeof(getpid())+strlen(argv[1]) +5 /*for commas and newline*/+sizeof(total_number));
+    char buffer_output[size_of_output_file];
+    
+    int fd;
+       /*
+        permissions is like  
+        r-- ->4                 so i want 644 to give rw--w--w-
+        -w- ->2
+        --x -<1
+    */
+    //create an output file and write on it... if already exists append that
+    if ((fd=open("output.txt",O_WRONLY | O_CREAT | O_APPEND,0644 ))==-1)
+    {
+        perror("");
+        exit(1);
+    }
+
+    //prepare the format of text we will print on output file
+    snprintf(buffer_output,size_of_output_file,"%d, %s, %d\n",getpid(),argv[1],total_number);
+
+    //write the array of characters in the file
+    write(fd, buffer_output, strlen(buffer_output)); 
+
+    return 0;
+}       
+
+//FUNCTIONS IMPLEMENTATIONS
+int word_seperator(char character)
+{
+    if(character==' ' ||character=='\n'||character=='\t'||character=='\0'
+     ||character==',' ||character=='!' ||character=='`' ||character=='('
+     ||character==')' ||character=='{' ||character=='}' ||character=='['
+     ||character==']' ||character=='+' ||character=='-' ||character=='>'
+     ||character=='<' ||character=='.' ||character=='%' ||character=='@' 
+     ||character=='*' ||character=='^' ||character=='*' ||character=='"'
+     ||character=='~' ||character=='/' ||character==';' ||character=='&'
+     ||character=='#' ||character=='$' ||character=='_' ||character==':'
+     ||character=='0' ||character=='1' ||character=='2' ||character=='3'
+     ||character=='4' ||character=='5' ||character=='6' ||character=='7'
+     ||character=='?' ||character=='8' ||character=='9' ||character=='\\'
+     ||character=='|')
+    {
+        return 1;//true,is a seperator
+
+    }
+    return 0;//false,its not a seperator
+}
+
+
+//counts the words of a file with the help of the threads
+void *counter_words_of_file(void *data_args)
+{
+    //take the data of the thread
+    data *args;
+    args =data_args;
+
+    //take the number of threads
+    int number_of_threads=args->thread_counter;
+
+    //make a counter for words of a thread
+    int counter=0;
+   // int i;
+    
+    //check if is the last thread to read correctly the lasts characters in buffer
+   int modulo=0;
+    if(number_of_threads==NTHREADS-1 && strlen(args->buffer)%NTHREADS!=0){
+       modulo=(strlen(args->buffer)%NTHREADS)-1;
+    }
+    for(int i=number_of_threads*(strlen(args->buffer)/NTHREADS);i<(number_of_threads+1)*(strlen(args->buffer)/NTHREADS)+modulo;i++)
+        {
+            //check if it is in the end of a word
+            if(!word_seperator(args->buffer[i]) && word_seperator(args->buffer[i+1]))
+            {
+                //add by one the counter of words
+                counter++;
+            }
+        }
+
+     // "freeze" threads, to control threads we have to  let one thread at the time
+    pthread_mutex_lock(&mutex);
+
+     //and then let that thread to add on sum of counter
+    total_number+=counter;
+
+    //and unlock - unfreeze threads
+    pthread_mutex_unlock(&mutex);
+
+    //exit of thread
+    pthread_exit (NULL);
+    
+}
---
+++ it219105.c
@@ -0,0 +1,199 @@
+#include <stdio.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <signal.h>
+
+//declare function, their implementation is on end
+void handle_sigint(int);
+void handle_sigterm(int);
+
+int main (int argc, char *argv[])
+{
+    //check if he gave more than one arguments.
+    if (argc>2){
+        printf("\tYou cannot give more than one arguments!");
+        return -1;
+    }
+    DIR *folder;
+    char *dir;
+    //if he gave me directory save his path otherwise save current directory
+    if (argc==2) //so he gave me an argument
+    {
+        //open given directory 
+        dir=argv[1];
+    }else{
+
+        //open current directory
+        dir="./";
+    }
+    folder = opendir(dir);
+    
+    //check if directory opened succesfully
+   if(folder == NULL)
+     {
+            puts("\tUnable to read directory");
+            exit(-1);
+     }
+       
+
+
+
+    //declare variables before while, so i don`t make unnecessary statements
+    struct dirent *pde;  // Pointer for Directory Entry 
+    char * name_of_file;  //use it to take file of each name
+    int name_of_file_has_size; //use it to take size of each file name 
+    DIR* check_is_file; //use it to check if it`s file
+    int  fd ; //use it to open files 
+    int number_of_characters; //use it to take the number of characters
+    long file_size; //use it to take the size of each file 
+    pid_t pid,ppid;
+    int forks=0;//sum of forks, so i can check for zombies
+
+
+
+    //I use readdir to read  this directory 
+    while ((pde = readdir(folder)) != NULL)
+    {
+         printf("%s\n", pde->d_name); 
+        //so now i have the names of whatever is inside of folder, one under the other. 
+        name_of_file= pde->d_name;
+        //use strlen to find the size of string-name of file 
+        name_of_file_has_size=strlen(dir) +1/*for new line*/ +strlen(name_of_file);  
+        //make a buffer for this string 
+        char buffer_for_name_of_file[name_of_file_has_size];
+        //use sprintf to printf a string (for that is the s on start)
+        sprintf(buffer_for_name_of_file,"%s/%s",dir,name_of_file);
+        //use opendir to check if it is a file
+        check_is_file= opendir(buffer_for_name_of_file);
+        if(check_is_file!=NULL) // so it`s not a file, go on next
+        {
+            printf("\t%s: it`s not a file!\n",pde->d_name);
+            continue;
+        }
+
+     
+
+        //open each file
+        fd=open(buffer_for_name_of_file, O_RDONLY); //we need Read only 
+        if(fd<0) //has to be possitive to be right
+        {
+            perror("open");
+            exit(1);
+        }
+       // printf("\tsuccessfully opened the fd=%d \n",fd);
+  
+       //calculate file size with lseek use
+        file_size= lseek(fd,0,SEEK_END); //now cursor accessed the entire file and stayed at end of it 
+        if(file_size==0)
+        {
+           printf("\tEmpty file!");
+           continue; //go to check for next file, you have not words to count here.
+        }
+        lseek(fd,0,SEEK_SET); //put cursor again on start of file
+    
+    
+    
+        //create a buffer to help us read the number of characters
+        char buff[file_size];
+        //take the number of characters using read()
+        number_of_characters=read(fd,buff,file_size); //so now buffer has the characters
+       
+       
+       
+        /*
+        use buffer (buff) to take characters and
+        check if each of them is on ASCII table.
+        if not then i print a message and i go on next file.
+        */
+        int found_no_Ascii=0;
+  
+        for(int i=0;i<file_size;i++)
+         {        //printf("buffer for %d prints ->%d\n\n",i,buff[i]);
+             if(buff[i]<0||buff[i]>127)//so it`s out of ASCII
+            {
+                 found_no_Ascii=1;
+            }
+        }
+        // printf("file size is %ld",file_size);
+        if(found_no_Ascii==1)
+        {
+            printf("\tThis file it`s not with ASCII characters!\n");
+            continue; //go to check for other files
+        }
+
+
+        //end of first part of the exercise (basic process)
+
+
+
+        //start of creation of new processes
+
+
+      /*
+         I am going to use an exec function
+         before that I had to create an array with arguments
+        -> the path of counting-program
+        -> the buffer of file name  
+        -> the buffer of that file
+        ->null, to mark the end of the array of pointers
+      */
+        char *args[]={"./counting_words",buffer_for_name_of_file,buff,NULL};
+
+       /*
+        I use fork to create a new process
+        for each  file (if a file is on this line
+        then sure is on ascii)
+       */
+        forks++;
+        pid=fork();
+       
+
+        if(pid<0) //check for fail creation
+        {
+            perror("creation of a child process was unsuccessful\n");
+            exit(1);
+        }
+        //check if returned child or parent
+        if(pid==0)//child
+        { 
+            //use execv to replace the prosses with another, and count the words of the file
+            execv(args[0],args);
+        }else{ //parent 
+            //use signal for hadling
+            signal(SIGINT, handle_sigint); 
+		    signal(SIGTERM,handle_sigterm);
+            signal(SIGCHLD,SIG_IGN);
+        }
+
+     //close file 
+      if(close(fd)<0)
+      {
+          perror("\tUnexpected error!Something went wrong with close.");
+          exit(1);
+      }
+    } 
+    //take care for zombies processes, wait to end all the children!
+    for(int i=0;i<forks;i++){
+        wait(NULL);
+    }
+     closedir(folder);
+     exit(1);
+return 0;
+}
+
+//FUNCTIONS FOR ERROR HANDLING
+void handle_sigterm(int signal) 
+{ 
+    printf("\n\tIgnored signal %d\n", signal); 
+} 
+
+void handle_sigint(int signal) 
+{ 
+    printf("\n\tIgnored signal %d\n", signal); 
+} 
